40ab4b62bb24aee930fad9dfb10be025d1780aea
Iván Manzaba <mauricio.9.inm@gmail.com>
2026-09-20 20:35:12 -0500

    add components structure and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth-guard';
+import { OrdersComponent } from './features/orders/orders-component';
+import { LoginComponent } from './features/login/login-component';
+import { ProductsComponent } from './features/products/products-component';
+import { CartComponent } from './features/cart/cart-component';
+import { CheckoutComponent } from './features/checkout/checkout-component';
+import { OrderDetailComponent } from './features/order-detail/order-detail-component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'products', component: ProductsComponent, canActivate: [authGuard] },
+  { path: 'cart', component: CartComponent, canActivate: [authGuard] },
+  { path: 'checkout', component: CheckoutComponent, canActivate: [authGuard] },
+  { path: 'orders', component: OrdersComponent, canActivate: [authGuard] },
+  { path: 'orders/:id', component: OrderDetailComponent, canActivate: [authGuard] },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '**', redirectTo: '/products' },
+];
---
+++ src/app/features/order-detail/order-detail-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-detail-component',
+  imports: [],
+  templateUrl: './order-detail-component.html',
+  styleUrl: './order-detail-component.css',
+})
+export class OrderDetailComponent {}
